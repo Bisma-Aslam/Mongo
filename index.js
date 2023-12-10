a540d856f6feee4a798e76c075dbfb4e4e5d6e2d
Bisma-Aslam <bismamalik2056@gmail.com>
2023-12-10 14:22:45 +0500

    added vercel

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const {
-  createBook,
-  getAllBooks,
-  deleteBook,
-  updateBook,
-  getBookById,
-} = require("./operations");
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const bookRouter = require('./routes/bookRoutes'); // Assuming you have a separate book router
+const bodyParser = require('body-parser');
 
-app.use(express.json());
+const mongoString = process.env.CONNURL;
 
-mongoose
-  .connect("mongodb://localhost:27017", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+mongoose.connect(mongoString, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(async () => {
     console.log("Connection to MongoDB created");
   })
@@ -24,57 +16,15 @@ mongoose
     console.log(err);
   });
 
-app.post("/books", async (req, res) => {
-  try {
-    const { title, author, genre } = req.body;
-    const newBook = await createBook(title, author, genre);
-    res.json(newBook);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api/books', bookRouter); // Adjust the route path to match your desired structure
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/books", async (req, res) => {
-  try {
-    const allBooks = await getAllBooks();
-    res.json(allBooks);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-app.get("/books/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const book = await getBookById(id);
-    res.json(book);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-app.put("/books/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { title, author, genre } = req.body;
-    const updatedBook = await updateBook(id, title, author, genre);
-    res.json(updatedBook);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-app.delete("/books/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const deletedBook = await deleteBook(id);
-    res.json(deletedBook);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-const PORT = 8000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// Select an available port
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log("Server started at port " + port + "");
 });
